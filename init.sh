e9c5612a839ab0e6abc9b2fdc906835a04401501
Ori Hoch <ori@uumpa.com>
2021-02-22 07:18:49 +0200

    add timeout to all curl commands --skip-tests

--- init.sh
+++ init.sh
@@ -3,9 +3,9 @@ echo init &&\
 if [ "${MINIO_AUDIT_WEBHOOK_ENDPOINT_target1}" != "" ] && [ "${SKIP_WAIT_FOR_AUDIT_WEBHOOK}" == "" ]; then
   echo waiting for audit webhook "${MINIO_AUDIT_WEBHOOK_ENDPOINT_target1}" &&\
   if [ "${CWM_INIT_DEBUG}" != "" ]; then
-    while ! curl -XPOST "${MINIO_AUDIT_WEBHOOK_ENDPOINT_target1}"; do sleep 1; done
+    while ! curl --max-time 0.05 -XPOST "${MINIO_AUDIT_WEBHOOK_ENDPOINT_target1}"; do sleep 1; done
   else
-    while ! curl -sXPOST "${MINIO_AUDIT_WEBHOOK_ENDPOINT_target1}"; do sleep .01; done
+    while ! curl --max-time 0.05 -sXPOST "${MINIO_AUDIT_WEBHOOK_ENDPOINT_target1}"; do sleep .01; done
   fi
 fi &&\
 echo setting ulimit &&\
