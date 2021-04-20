a833d854bae115564c06e794ed77d27307adbc81
Ori Hoch <ori@uumpa.com>
2021-04-20 11:20:43 +0300

    remove metrics-logger (now using cwm-worker-logger)

--- metrics-logger/metrics_logger/daemon.py
+++
@@ -1,6 +0,0 @@
-from metrics_logger.http_server import MetricsLoggerHTTPServer
-from metrics_logger.metrics_manager import MetricsManager
-
-
-def start():
-    MetricsLoggerHTTPServer(MetricsManager()).serve_forever()
