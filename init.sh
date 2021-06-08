471be6e621a8d13294416f8456070b9495db863b
Ori Hoch <ori@uumpa.com>
2021-06-08 11:13:44 +0300

    add support for azure and google gateways

--- init.sh
+++ init.sh
@@ -13,6 +13,16 @@ ulimit -n 1024000 &&\
 if [ "${INSTANCE_TYPE}" == "gateway_s3" ]; then
   echo starting gateway s3 http &&\
   exec /usr/bin/minio gateway s3 $MINIO_EXTRA_ARGS --address :8080 $GATEWAY_ARGS
+elif [ "${INSTANCE_TYPE}" == "gateway_gcs" ]; then
+  echo starting gateway gcs http &&\
+  export GOOGLE_APPLICATION_CREDENTIALS=/tmp/gcs_credentials.json &&\
+  echo "${GOOGLE_APPLICATION_CREDENTIALS_JSON}" > $GOOGLE_APPLICATION_CREDENTIALS &&\
+  exec /usr/bin/minio gateway gcs $MINIO_EXTRA_ARGS --address :8080 $GATEWAY_ARGS
+elif [ "${INSTANCE_TYPE}" == "gateway_azure" ]; then
+  echo starting gateway gcs http &&\
+  export MINIO_ROOT_USER="${AZURE_STORAGE_ACCOUNT_NAME}" &&\
+  export MINIO_ROOT_PASSWORD="${AZURE_STORAGE_ACCOUNT_KEY}" &&\
+  exec /usr/bin/minio gateway azure $MINIO_EXTRA_ARGS --address :8080
 else
   echo starting gateway nas http &&\
   exec /usr/bin/minio gateway nas $MINIO_EXTRA_ARGS --address :8080 /storage/
