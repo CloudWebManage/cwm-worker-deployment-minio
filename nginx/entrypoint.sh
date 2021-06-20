75be941b80bce05e4a8898605c7e81639a9d2d9f
Azeem Sajid <azeem.sajid@gmail.com>
2021-06-20 15:37:56 +0500

    Block http/https via nginx. (#20)
    * Block http/https via nginx.
    
    Signed-off-by: Azeem Sajid <azeem.sajid@gmail.com>
    
    * Enable http and https by default.

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 
-[ "${HOSTNAMES_DIR}" == "" ] && echo missing HOSTNAMES_DIR env var && exit 1
+[ "$HOSTNAMES_DIR" == "" ] && echo "missing HOSTNAMES_DIR env var" && exit 1
 
-/etc/nginx/init.sh "${HOSTNAMES_DIR}" "/etc/nginx" "/etc/nginx/conf.d" &&\
+NGINX_SOURCES_DIR="/etc/nginx"
+NGINX_CONFD_DIR="$NGINX_SOURCES_DIR/conf.d"
+
+[ "$DISABLE_HTTP" == "true" ] && rm -f "$NGINX_SOURCES_DIR/http.conf" && echo "HTTP is disabled!"
+[ "$DISABLE_HTTPS" == "true" ] && rm -f "$NGINX_SOURCES_DIR/https.conf" && echo "HTTPS is disabled!"
+
+/etc/nginx/init.sh "$HOSTNAMES_DIR" "$NGINX_SOURCES_DIR" "$NGINX_CONFD_DIR" &&\
 exec nginx -g "daemon off;"
--- nginx/init.sh
+++ nginx/init.sh
@@ -1,25 +1,40 @@
 #!/usr/bin/env bash
 
+set -e
+
 HOSTNAMES_DIR="${1}"
 NGINX_SOURCES_DIR="${2}"
 NGINX_CONFD_DIR="${3}"
 
-for filename in `ls "${HOSTNAMES_DIR}"/*.name`; do
+DEFAULT_HTTP_CONF="$NGINX_SOURCES_DIR/http.conf"
+DEFAULT_HTTPS_CONF="$NGINX_SOURCES_DIR/https.conf"
+
+for filename in $(ls "${HOSTNAMES_DIR}"/*.name); do
   filename="$(basename "${filename}")" &&\
   host_id="$(echo "${filename%.*}")" &&\
   host_name="$(cat "${HOSTNAMES_DIR}/${host_id}.name")" &&\
-  echo ${host_id} ${host_name} &&\
-  sed "s/__SERVER_NAME__/server_name ${host_name};/" "${NGINX_SOURCES_DIR}/http.conf" > "${NGINX_CONFD_DIR}/${host_id}-http.conf" &&\
-  key_filename="${HOSTNAMES_DIR}/${host_id}.key" &&\
-  pem_filename="${HOSTNAMES_DIR}/${host_id}.pem"
-  [ "$?" != "0" ] && echo failed to setup hostname && exit 1
-  if [ -e "${key_filename}" ] && [ -e "${pem_filename}" ]; then
-    sed "s/__SERVER_NAME__/server_name ${host_name};/" "${NGINX_SOURCES_DIR}/https.conf" > "${NGINX_CONFD_DIR}/${host_id}-https.conf" &&\
-    sed -i "s;__PEM__;${pem_filename};g" "${NGINX_CONFD_DIR}/${host_id}-https.conf" &&\
-    sed -i "s;__KEY__;${key_filename};g" "${NGINX_CONFD_DIR}/${host_id}-https.conf"
-    [ "$?" != "0" ] && echo failed to setup https && exit 1
+  echo "${host_id} ${host_name}"
+
+  if [ -f "$DEFAULT_HTTP_CONF" ]; then
+    http_conf="$NGINX_CONFD_DIR/$host_id-http.conf"
+    echo "setting up $http_conf"
+    sed "s/__SERVER_NAME__/server_name ${host_name};/" "$DEFAULT_HTTP_CONF" > "$http_conf"
+  fi
+
+  if [ -f "$DEFAULT_HTTPS_CONF" ]; then
+    key_filename="${HOSTNAMES_DIR}/${host_id}.key" &&\
+    pem_filename="${HOSTNAMES_DIR}/${host_id}.pem"
+    [ "$?" != "0" ] && echo "failed to setup hostname" && exit 1
+    if [ -f "${key_filename}" ] && [ -f "${pem_filename}" ]; then
+      https_conf="$NGINX_CONFD_DIR/$host_id-https.conf"
+      echo "setting up $https_conf"
+      sed "s/__SERVER_NAME__/server_name ${host_name};/" "$DEFAULT_HTTPS_CONF" > "$https_conf" &&\
+      sed -i "s;__PEM__;${pem_filename};g" "$https_conf" &&\
+      sed -i "s;__KEY__;${key_filename};g" "$https_conf"
+      [ "$?" != "0" ] && echo "failed to setup https" && exit 1
+    fi
   fi
 done
 
-echo init OK
+echo "init OK"
 exit 0
