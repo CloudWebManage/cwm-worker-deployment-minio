2cf0250eb0f0a718431e913d6e7f22dc32ef3621
Ori Hoch <ori@uumpa.com>
2021-08-30 12:28:28 +0300

    misc. CI improvements, switch to public docker packages

--- bin/install_kubectl.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-cd `mktemp -d` &&\
-curl -LO "https://storage.googleapis.com/kubernetes-release/release/v1.19.0/bin/linux/amd64/kubectl" &&\
-chmod +x ./kubectl && mv ./kubectl /usr/local/bin/kubectl
