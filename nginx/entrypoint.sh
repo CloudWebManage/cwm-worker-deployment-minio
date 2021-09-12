64c7cf90cd28af4f5ec8527ead4ac238127cb758
Ori Hoch <ori@uumpa.com>
2021-09-12 21:11:28 +0300

    fix for nginx cache if-match header, add support for nginx debug mode, improve docker compose self-gateway

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -8,5 +8,13 @@ NGINX_CONFD_DIR="$NGINX_SOURCES_DIR/conf.d"
 [ "$DISABLE_HTTP" == "true" ] && echo "HTTP is disabled!"
 [ "$DISABLE_HTTPS" == "true" ] && rm -f "$NGINX_SOURCES_DIR/https.conf" && echo "HTTPS is disabled!"
 
+if [ "${NGINX_DEBUG}" == "yes" ]; then
+  echo Running nginx in debug mode
+  sed -i "s;error_log /var/log/nginx/error.log warn;error_log /var/log/nginx/error.log debug;g" "${NGINX_SOURCES_DIR}/nginx.conf"
+  nginx_bin="nginx-debug"
+else
+  nginx_bin="nginx"
+fi
+
 /etc/nginx/init.sh "$HOSTNAMES_DIR" "$NGINX_SOURCES_DIR" "$NGINX_CONFD_DIR" &&\
-exec nginx -g "daemon off;"
+exec "${nginx_bin}" -g "daemon off;"
