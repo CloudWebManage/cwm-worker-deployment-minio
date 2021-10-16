00e837650b8bda2096f72b484ee23bbf31a5616d
Azeem Sajid <azeem.sajid@gmail.com>
2021-10-16 08:41:51 +0500

    [CWM-154] Upgrade minio to latest version.
    Signed-off-by: Azeem Sajid <azeem.sajid@gmail.com>

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 # this base image should use a release date close to the base image in client/Dockerfile
-FROM minio/minio:RELEASE.2021-06-09T18-51-39Z@sha256:88931f679647720b738849402abc841b2288513dc4d2821b1891c0f27e1d644d
+FROM minio/minio:latest
 COPY init.sh /root/init.sh
 ENTRYPOINT ["sh"]
 CMD ["/root/init.sh"]
--- init.sh
+++ init.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
+
 echo init &&\
 if [ "${MINIO_AUDIT_WEBHOOK_ENDPOINT_target1}" != "" ] && [ "${SKIP_WAIT_FOR_AUDIT_WEBHOOK}" == "" ]; then
-  echo waiting for audit webhook "${MINIO_AUDIT_WEBHOOK_ENDPOINT_target1}" &&\
+  echo "waiting for audit webhook ${MINIO_AUDIT_WEBHOOK_ENDPOINT_target1}" &&\
   if [ "${CWM_INIT_DEBUG}" != "" ]; then
     while ! curl --fail --show-error --max-time "${CWM_INIT_CURL_MAX_TIME:-1.5}" -XPOST --data '{}' "${MINIO_AUDIT_WEBHOOK_ENDPOINT_target1}"; do sleep 1; done
   else
@@ -11,19 +12,21 @@ fi &&\
 echo setting ulimit &&\
 ulimit -n 1024000 &&\
 if [ "${INSTANCE_TYPE}" == "gateway_s3" ]; then
-  echo starting gateway s3 http &&\
-  exec /usr/bin/minio gateway s3 $MINIO_EXTRA_ARGS --address :8080 $GATEWAY_ARGS
+  echo "starting gateway s3 http" &&\
+  exec /opt/bin/minio gateway s3 $MINIO_EXTRA_ARGS --address ":8080" --console-address ":8081" $GATEWAY_ARGS
 elif [ "${INSTANCE_TYPE}" == "gateway_gcs" ]; then
-  echo starting gateway gcs http &&\
+  echo "starting gateway gcs http" &&\
   export GOOGLE_APPLICATION_CREDENTIALS=/tmp/gcs_credentials.json &&\
   echo "${GOOGLE_APPLICATION_CREDENTIALS_JSON}" > $GOOGLE_APPLICATION_CREDENTIALS &&\
-  exec /usr/bin/minio gateway gcs $MINIO_EXTRA_ARGS --address :8080 $GATEWAY_ARGS
+  exec /opt/bin/minio gateway gcs $MINIO_EXTRA_ARGS --address ":8080" --console-address ":8081" $GATEWAY_ARGS
 elif [ "${INSTANCE_TYPE}" == "gateway_azure" ]; then
-  echo starting gateway azure http &&\
+  echo "starting gateway azure http" &&\
   export AZURE_STORAGE_ACCOUNT="${AZURE_STORAGE_ACCOUNT_NAME}" &&\
   export AZURE_STORAGE_KEY="${AZURE_STORAGE_ACCOUNT_KEY}" &&\
-  exec /usr/bin/minio gateway azure $MINIO_EXTRA_ARGS --address :8080
+  exec /opt/bin/minio gateway azure $MINIO_EXTRA_ARGS --address ":8080" --console-address ":8081"
 else
-  echo starting gateway nas http &&\
-  exec /usr/bin/minio gateway nas $MINIO_EXTRA_ARGS --address :8080 /storage/
-fi
+  echo "starting gateway nas http" &&\
+  exec /opt/bin/minio gateway nas $MINIO_EXTRA_ARGS --address ":8080" --console-address ":8081" /storage/
+fi &&\
+export MINIO_BROWSER_REDIRECT_URL="http://127.0.0.1:8081/"
+# export MINIO_SERVER_URL="https://127.0.0.1:8080/"
